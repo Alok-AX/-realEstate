5a406b8eff6a09103b1133c929e0c461ed3e1ca8
Alok <alok965801@gmail.com>
2024-04-20 07:52:54 +0530

    creating pages with installed react-roter-dom and all pages initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import SignOut from "./pages/SignOut"
+import Profile from "./pages/Profile"
 export default function App() {
   return (
-    <div>
-       <h1 className='text-red-400'>Hello</h1>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-out' element={<SignOut/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  
+  </BrowserRouter>
   )
 }
